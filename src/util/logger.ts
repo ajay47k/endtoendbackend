d24c2cb26d8d5beae116c2ef356f598ae0c02d52
Ajay Sengar <ajay.senger2@gmail.com>
2025-05-27 11:00:37 -0400

    feat: Colorize terminal

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -5,21 +5,35 @@ import { EApplicationEnvironment } from '../constant/application'
 import config from '../config/config'
 import path from 'path'
 import * as sourceMapSupport from 'source-map-support'
+import { red, blue, yellow, green, magenta } from 'colorette'
 
 // Linking trace support
 sourceMapSupport.install()
 
+const colorizeLevel = (level: string) => {
+    switch (level) {
+        case 'ERROR':
+            return red(level)
+        case 'INFO':
+            return blue(level)
+        case 'WARN':
+            return yellow(level)
+        default:
+            return level
+    }
+}
+
 const consoleLogFormat = format.printf((info) => {
     const { level, message, timestamp, meta } = info
 
-    const customLevel = level.toUpperCase()
-    const customTimestamp = timestamp
+    const customLevel = colorizeLevel(level.toUpperCase())
+    const customTimestamp = green(timestamp as string)
     const customMessage = message
     const customMeta = util.inspect(meta, {
         showHidden: false,
         depth: null
     })
-    const customLog = `${customLevel} [${customTimestamp}] ${customMessage}\n ${'META'} ${customMeta}`
+    const customLog = `${customLevel} [${customTimestamp}] ${customMessage}\n ${magenta('META')} ${customMeta}`
     return customLog
 })
 
